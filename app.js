b9c5f31cf65522098de35967da2e809bca9fdb1a
unknown <Myrienestor@gmail.com>
2021-07-20 20:03:30 -0600

    aa

---
+++ app.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('canvas');
+const w = document.body.clientWidth;
+const h = document.body.clientHeight;
+canvas.width =w;
+canvas.height=h;
+const position = Array(300).join(0).split('');
+const ctx = canvas.getContext('2d');
+
+const logica = () => {
+  ctx.fillStyle= 'rgba(0,15,2,.15)'
+  ctx.fillRect(0,0,w,h)
+  ctx.fillStyle='#4caf50'
+  ctx.font='12px'
+
+  position.map((y,index) => {
+    const text = String.fromCharCode(1e3 +Math.random() *30)
+    const x =index *15 + 15
+    canvas.getContext('2d').fillText(text,x,y)
+
+    y > 10 + Math.random() * 1e5? position[index]=0 : position[index] =y + 15
+  })
+}
+
+setInterval(logica,70)
